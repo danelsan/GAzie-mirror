6343c8dfc59d1b7db14abe5367be1d64e6e78f69
tonybio <tonybio>
2025-05-29 06:19:24 +0000

    VAcation rental: corretto primo accesso al report booking che non selezionava l'anno più recente

--- modules/vacation_rental/report_booking.php
+++ modules/vacation_rental/report_booking.php
@@ -115,7 +115,6 @@ if (isset ($_GET['inevasi'])){
 	$form['swStatus']=(isset($_GET['swStatus']))?$_GET['swStatus']:'';
 }
 
-
 $partner_select = !gaz_dbi_get_row($gTables['company_config'], 'var', 'partner_select_mode')['val'];
 $tesbro_e_partners = "{$gTables['tesbro']} LEFT JOIN {$gTables['clfoco']} ON {$gTables['tesbro']}.clfoco = {$gTables['clfoco']}.codice LEFT JOIN {$gTables['anagra']} ON {$gTables['clfoco']}.id_anagra = {$gTables['anagra']}.id";
 $tesbro_e_destina = $tesbro_e_partners . " LEFT JOIN {$gTables['destina']} ON {$gTables['tesbro']}.id_des_same_company = {$gTables['destina']}.codice";
@@ -173,13 +172,18 @@ unset($terzo);
 if (isset($form['swStatus']) AND $form['swStatus']=="Inevasi"){
 	$passo=1000;
 }
+if (!isset($_GET['auxil'])){
+	$auxil='VOR';
+}else{
+	$auxil=$_GET['auxil'];
+}
 if (count($_GET)<=1){
 	// ultimo documento
 	$rs_last = gaz_dbi_dyn_query('seziva, YEAR(datemi) AS yearde', $gTables['tesbro'], "tipdoc LIKE '".substr($auxil,0,3)."'", 'datemi DESC, id_tes DESC', 0, 1);
 	$last = gaz_dbi_fetch_array($rs_last);
 	if ($last) {
 		$default_where=['sezione' => $last['seziva'], 'tipo' => 'F%', 'anno'=>$last['yearde']];
-        $_GET['anno']=$last['yearde'];
+        $_GET['anno']=$last['yearde'];		
 	} else {
 		$default_where= ['auxil' => 'VOR'];
 	}
@@ -1606,11 +1610,14 @@ $ts->output_navbar();
                       $ultimo_documento = 0;
                       mostra_documenti_associati( $r['id_tes'], $paid );
                   }
-                  ?>
-                  <a style="float:right;" title="Genera pdf contratto" class="btn btn-xs dialog_leasecr" ref="<?php echo $r['id_tes']; ?>" nome="<?php echo $r['ragso1']; ?>" url=<?php echo "stampa_contratto.php?id_tes=". $r['id_tes'] . "&id_ag=". $r['id_agent']; ?>>
-                    <i class="glyphicon glyphicon-refresh"></i>
-                  </a>
-                  <?php
+				  
+                  if ($r['status']=='CONFIRMED'){                         
+					  ?>
+					  <a style="float:right;" title="Genera pdf contratto" class="btn btn-xs dialog_leasecr" ref="<?php echo $r['id_tes']; ?>" nome="<?php echo $r['ragso1']; ?>" url=<?php echo "stampa_contratto.php?id_tes=". $r['id_tes'] . "&id_ag=". $r['id_agent']; ?>>
+						<i class="glyphicon glyphicon-refresh"></i>
+					  </a>
+					  <?php
+				  }
                  echo "</td>";
               }elseif(isset($datatesbro['vacation_rental']['id_booking']) && intval($datatesbro['vacation_rental']['id_booking'])>0){
                 echo "<td><a class=\"btn btn-xs btn-warning\" href=\"../../modules/vacation_rental/report_booking.php?info=none&auxil=VOR&id_doc=" . intval($datatesbro['vacation_rental']['id_booking']) . "\">Prenotazione effettuata</a></td>";

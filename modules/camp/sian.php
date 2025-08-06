0ef0670a975b8b2cdcb15b4fbb670440c63e2a41
devincen <devincen>
2025-08-06 14:16:30 +0000

    Continuo con il rendere più verbosi gli alert per facilitare la correzione di eventuali errori

--- modules/camp/sian.php
+++ modules/camp/sian.php
@@ -305,11 +305,11 @@ if (isset($_POST['preview']) and $msg=='') {
 								$totcont[$mv['recip_stocc']] -= floatval($mv['quanti']);
 								//echo "<br><br>PRODUZIONE SCarico fusto ",$mv['recip_stocc']," di:",$mv['quanti']," totale recipiente:",$totcont[$mv['recip_stocc']]," - datdoc:",$mv['datdoc'];
                 if (strlen($mv['recip_stocc']) < 1 ){
-									$message .= ' Il movimento di magazzino con ID = '.$mv['id_mov'].' non ha un silos valido\n';
+									$message .= 'Rigo '.$nr.', movimento di magazzino con ID '.$mv['id_mov'].' non ha il silos sul movimento SIAN ID '.$mv['id_mov_sian'].'\n';
 									$msg .='5+';$er="style='background-color: red';";
 								} else if ($totcont[$mv['recip_stocc']]<0){
 									//echo "<br>",$mv['desdoc'],"ERRORE <",$nr;
-									$message .= "Al rigo ".$nr." movimento magazzino ID: ".$mv['id_mov']." la giacenza del silos ".$mv['recip_stocc'].' è negativa\n';
+									$message .= "Rigo ".$nr.", movimento magazzino ID: ".$mv['id_mov']." la giacenza del silos ".$mv['recip_stocc'].' è negativa\n';
 									$msg .='5+';$er="style='background-color: red';";
 								}
 
